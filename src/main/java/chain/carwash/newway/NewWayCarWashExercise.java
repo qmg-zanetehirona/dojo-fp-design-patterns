95b6893c7765828673b57dccd9dabeb8693eac4e
Zanete Hirona <zmolina@policyexpert.co.uk>
2024-05-23 15:02:26 +0100

    add chain carwash exercise lambda

---
+++ src/main/java/chain/carwash/newway/NewWayCarWashExercise.java
@@ -0,0 +1,12 @@
+package chain.carwash.newway;
+
+import chain.carwash.Car;
+import chain.carwash.WashState;
+
+public class NewWayCarWashExercise {
+
+    public WashState getCarWashFinalState(Car car) {
+
+        return WashState.DRIED;
+    }
+}
